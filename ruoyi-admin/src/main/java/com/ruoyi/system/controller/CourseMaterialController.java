cdc7fada937d020557651986493345687b27b464
brandon <3440399961@qq.com>
2024-10-28 21:24:30 +0800

    注解添加2

--- ruoyi-admin/src/main/java/com/ruoyi/system/controller/CourseMaterialController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/system/controller/CourseMaterialController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
+import io.swagger.annotations.ApiImplicitParams;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,13 +19,16 @@ import com.ruoyi.system.service.ICourseMaterialService;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
 import org.springframework.web.multipart.MultipartFile;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 
 /**
- * 【请填写功能名称】Controller
- * 
- * @author ruoyi
+ * 课程资料管理Controller
+ *
  * @date 2024-10-27
  */
+@Api("课程资料管理")
 @RestController
 @RequestMapping("/system/course_material")
 public class CourseMaterialController extends BaseController
@@ -32,11 +36,11 @@ public class CourseMaterialController extends BaseController
     @Autowired
     private ICourseMaterialService courseMaterialService;
 
-
-
     /**
-     * 查询【请填写功能名称】列表
+     * 查询课程资料列表
      */
+    @ApiOperation("获取所有课程资料")
+    @ApiImplicitParam(name = "courseMaterial", value = "CourseMaterial", required = true, dataType = "CourseMaterial")
     @PreAuthorize("@ss.hasPermi('system:material:list')")
     @GetMapping("/list")
     public TableDataInfo list(CourseMaterial courseMaterial)
@@ -47,21 +51,25 @@ public class CourseMaterialController extends BaseController
     }
 
     /**
-     * 导出【请填写功能名称】列表
+     * 导出课程资料列表
      */
+    @ApiOperation("导出课程资料列表")
+    @ApiImplicitParam(name = "courseMaterial", value = "CourseMaterial", required = true, dataType = "CourseMaterial")
     @PreAuthorize("@ss.hasPermi('system:material:export')")
-    @Log(title = "【请填写功能名称】", businessType = BusinessType.EXPORT)
+    @Log(title = "课程资料管理", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
     public void export(HttpServletResponse response, CourseMaterial courseMaterial)
     {
         List<CourseMaterial> list = courseMaterialService.selectCourseMaterialList(courseMaterial);
         ExcelUtil<CourseMaterial> util = new ExcelUtil<CourseMaterial>(CourseMaterial.class);
-        util.exportExcel(response, list, "【请填写功能名称】数据");
+        util.exportExcel(response, list, "课程资料数据");
     }
 
     /**
-     * 获取【请填写功能名称】详细信息
+     * 获取课程资料详细信息
      */
+    @ApiOperation("获取课程资料详细信息")
+    @ApiImplicitParam(name = "materialId", value = "Material ID", required = true, dataType = "long", paramType = "path")
     @PreAuthorize("@ss.hasPermi('system:material:query')")
     @GetMapping(value = "/{materialId}")
     public AjaxResult getInfo(@PathVariable("materialId") Long materialId)
@@ -69,6 +77,13 @@ public class CourseMaterialController extends BaseController
         return success(courseMaterialService.selectCourseMaterialByMaterialId(materialId));
     }
 
+    /**
+     * 新增课程资料
+     */
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "file", value = "File", required = true, dataType = "MultipartFile", paramType = "form"),
+    @ApiImplicitParam(name = "courseId", value = "Course ID", required = true, dataType = "long", paramType = "form")
+    })
     @PreAuthorize("@ss.hasPermi('system:material:add')")
     @Log(title = "课程资料", businessType = BusinessType.INSERT)
     @PostMapping
@@ -84,10 +99,12 @@ public class CourseMaterialController extends BaseController
     }
 
     /**
-     * 修改【请填写功能名称】
+     * 修改课程资料
      */
+    @ApiOperation("修改课程资料")
+    @ApiImplicitParam(name = "courseMaterial", value = "CourseMaterial", required = true, dataType = "CourseMaterial")
     @PreAuthorize("@ss.hasPermi('system:material:edit')")
-    @Log(title = "【请填写功能名称】", businessType = BusinessType.UPDATE)
+    @Log(title = "课程资料管理", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody CourseMaterial courseMaterial)
     {
@@ -95,11 +112,13 @@ public class CourseMaterialController extends BaseController
     }
 
     /**
-     * 删除【请填写功能名称】
+     * 删除课程资料
      */
+    @ApiOperation("删除课程资料")
+    @ApiImplicitParam(name = "materialIds", value = "Material IDs", required = true, dataType = "array", paramType = "path")
     @PreAuthorize("@ss.hasPermi('system:material:remove')")
-    @Log(title = "【请填写功能名称】", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{materialIds}")
+    @Log(title = "课程资料管理", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{materialIds}")
     public AjaxResult remove(@PathVariable Long[] materialIds)
     {
         return toAjax(courseMaterialService.deleteCourseMaterialByMaterialIds(materialIds));
