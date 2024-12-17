43bbe63e8fb080a5c12990da784046268ef44748
TRM-coding <956303669@qq.com>
2024-12-17 22:29:17 +0800

    新分支

--- README.md
+++
@@ -1,1 +0,0 @@
-# Intelligent_course_platform_allstack
--- ruoyi-ui/src/api/system/folderStructure.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getFolderStructure(courseId) {
-  return request({
-    url: '/system/folder/structure' ,
-    method: 'get',
-    params: {courseId}
-  })
-}
